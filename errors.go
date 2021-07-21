7c66f4c9aa51be42a39ee6374e977cf82de26669
salvatoredangelo <salvatoredangelo@pop-os.localdomain>
2021-07-21 17:32:25 +0200

    more example

---
+++ errors.go
@@ -0,0 +1,28 @@
+package main
+
+import {
+	"errors"
+	"fmt"
+}
+
+func f1(arg int) (int, error){
+	if arg == 42{
+		return -1, errors.New("can't work with 42!")
+	}
+	return arg+3, nil
+}
+
+type argError struct {
+	arg int
+	prob string
+}
+
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
+
+func f2(arg int) (int, error) {
+	if arg ==42 {
+		return -1,
+	}
+}
